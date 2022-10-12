6a8b492c49116a1ae6799f2e8f84aeeec6217a06
Noor <noor.offl@gmail.com>
2022-10-13 01:25:41 +0530

    cleaned

--- app/src/main/java/com/logibix/demo/MainActivity.kt
+++ app/src/main/java/com/logibix/demo/MainActivity.kt
@@ -15,18 +15,5 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val students = arrayListOf<Student>()
-
-        students.add(Student("Shan", "Grad", 1001))
-        students.add(Student("Shub", "Madhyamik", 1002))
-        students.add(Student("Anu", "Twelve", 1003))
-        students.add(Student("Noor", "A", 1004))
-        students.add(Student("XYhan", "C", 1006))
-
-        val adapter = CustomAdapter(this, students)
-        binding.lvList.adapter = adapter
-        binding.lvList.setOnItemClickListener { adapterView, view, i, l ->
-            Toast.makeText(this, "Item is clicked", Toast.LENGTH_SHORT).show()
-        }
     }
 }
