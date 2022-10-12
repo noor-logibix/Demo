d8a6d774caef8e32872f59890add6682a2c2c0fb
Noor <noor.logibix@gmail.com>
2022-10-12 13:26:28 +0530

    Initial commit

---
+++ app/src/main/java/com/logibix/demo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.logibix.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.logibix.demo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
