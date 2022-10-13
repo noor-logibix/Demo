6b58699a3c03a3e204b8f4947758f1d8b8844f6f
Noor <noor.logibix@gmail.com>
2022-10-13 13:56:18 +0530

    RecyclerView added

--- app/src/main/java/com/logibix/demo/MainActivity.kt
+++ app/src/main/java/com/logibix/demo/MainActivity.kt
@@ -3,8 +3,10 @@ package com.logibix.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.core.view.get
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.logibix.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +17,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val students = arrayListOf<Student>()
+        students.add(Student("Shan", "Grad", 1001))
+        students.add(Student("Shub", "Madhyamik", 1002))
+        students.add(Student("Anu", "Twelve", 1003))
+        students.add(Student("Noor", "A", 1004))
+        students.add(Student("XYhan", "C", 1006))
+
+        for (i in 1007..2000) students.add(
+            Student(
+                (65 + (i % 26)).toChar().toString(),
+                (97 + ((i + 5) % 26)).toChar().toString(),
+                i
+            )
+        )
+
+
+        binding.rvList.layoutManager = LinearLayoutManager(this)
+        binding.rvList.adapter = StudentAdapter(students)
     }
 }
---
+++ app/src/main/java/com/logibix/demo/StudentAdapter.kt
@@ -0,0 +1,26 @@
+package com.logibix.demo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.logibix.demo.databinding.ListItemBinding
+
+class StudentAdapter(val students: ArrayList<Student>) :
+    RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StudentViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        holder.binding.tvName.text = students[position].name
+        holder.binding.tvStandard.text = students[position].standard
+        holder.binding.tvRollNo.text = students[position].rollNo.toString()
+    }
+
+    override fun getItemCount() = students.size
+
+    class StudentViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
+}
