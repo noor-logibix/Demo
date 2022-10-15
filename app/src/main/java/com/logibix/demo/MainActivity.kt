71db84fadb055e43789ce4dc96cc3883647e0255
Noor <noor.logibix@gmail.com>
2022-10-15 17:57:35 +0530

    Student created

--- app/src/main/java/com/logibix/demo/MainActivity.kt
+++ app/src/main/java/com/logibix/demo/MainActivity.kt
@@ -2,7 +2,10 @@ package com.logibix.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.logibix.demo.databinding.ActivityMainBinding
+import com.logibix.demo.databinding.AddStudentDialogBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val students = ArrayList<Student>()
+
+        binding.fabAdd.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val dialogBinding = AddStudentDialogBinding.inflate(layoutInflater)
+            val dialog = builder.setView(dialogBinding.root).create()
+
+            dialogBinding.btnAdd.setOnClickListener {
+                students.add(Student(
+                    dialogBinding.etName.text.toString(),
+                    dialogBinding.etClass.text.toString(),
+                    dialogBinding.etRollNumber.text.toString().toInt()
+                ))
+                dialog.dismiss()
+            }
+
+            dialogBinding.btnCancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.show()
+        }
+
+        binding.rvList.layoutManager = LinearLayoutManager(this)
+        binding.rvList.adapter = StudentAdapter(students)
     }
 }
---
+++ app/src/main/java/com/logibix/demo/Student.kt
@@ -0,0 +1,3 @@
+package com.logibix.demo
+
+data class Student(val name: String, val standard: String, val rollNo: Int)
