f2598c60e6edc05ddf858187882ff8ece37c39ec
Noor <noor.logibix@gmail.com>
2022-10-17 14:19:07 +0530

    SharedPreferences added

--- app/src/main/java/com/logibix/demo/MainActivity.kt
+++ app/src/main/java/com/logibix/demo/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.logibix.demo.databinding.ActivityMainBinding
 
+private const val MY_PREF = "myPref"
+
+private const val NAME = "name"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
@@ -11,5 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val preferences = getSharedPreferences(MY_PREF, MODE_PRIVATE)
+        val name = preferences.getString(NAME, "Null")
+
+        binding.tvText.text = "Hello $name"
+
+        binding.btnSave.setOnClickListener {
+            val preferences = getSharedPreferences(MY_PREF, MODE_PRIVATE)
+            val editor = preferences.edit()
+            editor .putString(NAME, binding.etName.text.toString())
+            editor.apply()
+            binding.tvText.text = "Hello ${binding.etName.text}"
+        }
     }
 }
+
+
